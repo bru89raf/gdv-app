71231915d2ec59e19d7acbba6e1c72cf3951dd1c
Bruno Oliveira <bru89raf@gmail.com>
2021-12-14 14:31:59 +0000

    Fix msg text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Grupo Despotivo de Valderde"/>
+    <HelloWorld msg="Grupo Desportivo de Valverde"/>
   </div>
 </template>
 
